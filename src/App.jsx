841b9a2be3121a4a6fc3ec05eb4c3824ae94d534
Dio <47392641@fi.unju.edu.ar>
2026-05-06 22:13:46 -0300

    Añado Nav como componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Footer } from "./components/Footer.jsx";
 import { Header } from "./components/Header.jsx";
+import { Nav } from "./components/Nav.jsx";
 import { ListaProyectos } from "./components/ListaProyectos.jsx";
 
 function App() {
   return (
     <>
       <Header />
+      <Nav activo="2" />
       <main>
         <ListaProyectos />
       </main>
