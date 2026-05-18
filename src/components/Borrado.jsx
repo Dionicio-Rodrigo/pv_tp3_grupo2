0d0eed8cc0f8fa83133f45d3d6ab72a2f24e8335
Dio <47392641@fi.unju.edu.ar>
2026-05-18 14:50:27 -0300

    Quito archivo y lineas innecesarias

--- src/components/Borrado.jsx
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export const Borrado = ({funcion,id}) => {
-    return (
-        <button className="btn-borrar" onClick={() => funcion(id)}>Eliminar Proyecto</button>
-    );
-};
