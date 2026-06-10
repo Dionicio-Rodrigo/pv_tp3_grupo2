7df6b3ae0fce595c6a40b4357dce4fdd686d5eae
Dio <47392641@fi.unju.edu.ar>
2026-06-10 19:06:52 -0300

    Añado y configuro el contexto para toda la pagina

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Theme from "./layouts/Theme.jsx";
+import { UsuarioProvider } from "./context/UsuarioContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider theme={Theme}>
       <CssBaseline />
-      <App />
+      <UsuarioProvider>
+        <App />
+      </UsuarioProvider>
     </ThemeProvider>
   </StrictMode>,
 );
