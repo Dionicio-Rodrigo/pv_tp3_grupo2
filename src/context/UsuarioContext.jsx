6162719442286c5fbe83c0ce74b186fb008ffdde
rmelinacruzb <r.melinacruz.b2@gmail.com>
2026-06-10 22:53:52 -0300

    Estado Global en Local Storage

--- src/context/UsuarioContext.jsx
+++ src/context/UsuarioContext.jsx
@@ -1,4 +1,6 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+
+const StorageKey = "sesionusuario"
 
 const UserContext = createContext();
 
@@ -10,10 +12,19 @@ export const UsuarioProvider = ({ children }) => {
     institucion: "Facultad de Ingenieria",
   };
 
-  const [usuario, setUsuario] = useState(datosPorDefecto);
+  const [usuario, setUsuario] = useState(() => {
+    const datosGuardados = localStorage.getItem(StorageKey);
+    return datosGuardados ? JSON.parse(datosGuardados) : datosPorDefecto;
+  });
+
+  useEffect(() => {
+    localStorage.setItem (StorageKey, JSON.stringify(usuario)); 
+    }, [usuario]);
+
   const actualizarDatos = (nuevosDatos) => {
-    setUsuario({ ...nuevosDatos });
+    setUsuario((Guardado) => ({ ...Guardado, ...nuevosDatos }));
   };
+  
   return (
     <UserContext.Provider value={{ usuario, actualizarDatos }}>
       {children}
