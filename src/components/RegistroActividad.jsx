620c87571a90334dd2cbbf74806198de8a34ee5b
Dio <47392641@fi.unju.edu.ar>
2026-06-13 17:37:49 -0300

    Corrigo el formato de la alerta

--- src/components/RegistroActividad.jsx
+++ src/components/RegistroActividad.jsx
@@ -7,19 +7,17 @@ export const RegistroActividad = ({ tiempo, mensaje }) => {
       variant="filled"
       sx={{ color: "colores.blanco", textShadow: "1px 1px  #292f36" }}
     >
-      {mensaje == "Agregado"
-        ? `Se agrego un nuevo proyecto a las `
-        : `Se elimino un proyecto a las `}
-      {new Intl.DateTimeFormat("es-AR", {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: false,
-      }).format(tiempo)}
-      {` `} del {` `}
+      Ultima actualización de la Lista a las: {``}
       {new Intl.DateTimeFormat("es-AR", {
         day: "2-digit",
         month: "2-digit",
         year: "numeric",
+      }).format(tiempo)}
+      {` `} del {` `}
+      {new Intl.DateTimeFormat("es-AR", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
       }).format(tiempo)}
     </Alert>
   );
